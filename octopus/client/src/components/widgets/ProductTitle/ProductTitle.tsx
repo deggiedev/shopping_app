a185a052b40843c68913d1a71c7d1c59c13e0e0e
Jack <42969795+deggiedev@users.noreply.github.com>
2022-04-03 16:28:11 +0100

    feat: implement Product Title with passing unit test

--- octopus/client/src/components/widgets/ProductTitle/ProductTitle.tsx
+++ octopus/client/src/components/widgets/ProductTitle/ProductTitle.tsx
@@ -1,6 +1,55 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
-export const ProductTitle: React.FC = () => {
-  return <Box>Product Title implementation to be made</Box>;
+interface Props {
+  title: string;
+  subTitle?: string;
+}
+export const ProductTitle: React.FC<Props> = ({ title, subTitle }) => {
+  
+  const has = {
+    subTitle: !!subTitle,
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alingContent: "center",
+        justifyContent: "center",
+        paddingTop: 4,
+        paddingBottom: 2,
+        paddingX: 1.5,
+        background: `#01193b`,
+      }}
+    >
+      <Typography
+        sx={{
+          lineHeight: "1.25",
+          fontSize: "30px",
+          fontFamily: "Gotham",
+          fontWeight: 900,
+          marginLeft: 3,
+          color: "white",
+        }}
+      >
+        {title}
+      </Typography>
+      {has.subTitle && (
+        <Typography
+          variant="subtitle1"
+          sx={{
+            fontFamily: "Gotham",
+            marginLeft: 3,
+            marginTop: 1,
+            color: "#335689",
+          }}
+        >
+          {subTitle}
+        </Typography>
+      )}
+    </Box>
+  );
 };
+
--- octopus/client/src/views/ProductView.tsx
+++ octopus/client/src/views/ProductView.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { Header } from "../components/widgets/Header";
+import { ProductTitle } from "../components/widgets/ProductTitle";
 
 export const ProductView: React.FC = () => {
   return (
     <Box>
       <Header></Header>
+      <ProductTitle
+        title={"Energy saving light bulb"}
+        subTitle={"25W // Packet of 4"}
+      />
     </Box>
   );
 };
